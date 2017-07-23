2a43b52ea0deceaf8fa979317e8268cc9416a7c8
nanoTitan <robertshd@hotmail.com>
2017-07-22 20:13:51 -0600

    Adding X-CUBE-MEMES drivers

---
+++ SprokytFirmware/X-CUBE-MEMS1/Middlewares/ST/STM32_OSX_MotionFX_Library/osx_license.h
@@ -0,0 +1,32 @@
+/**
+  ******************************************************************************
+  * Author  VMA Application Team (mv737)
+  * Version V1.0.7
+  * Date    4-April-2016
+  * Brief   Node-locked license for osx_motion_fx module
+  ******************************************************************************
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  ******************************************************************************
+  */
+
+/* Below line has to be removed once the license has been edited with valid codes */
+
+#ifndef _OSX_MOTION_FX_LICENSE_
+#define _OSX_MOTION_FX_LICENSE_
+uint32_t osx_mfx_license[3][4] =
+{
+	{ 0x0B2F347A, 0x38CB9ED2, 0x8431519B, 0x17F02020 },
+	{ 0x13A9E41A, 0x14328C1A, 0x5131D6E1, 0x39EDEEBD },
+	{ 0xFD8194B1, 0x13F86A42, 0xBF87AB4F, 0xE9160ED3 }
+}; 
+
+#endif // _OSX_MOTION_FX_LICENSE_
